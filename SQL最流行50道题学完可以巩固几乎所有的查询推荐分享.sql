4ffc0d002051ac9e0701154482cd9305ccaaa1b1
lstyxr <526560086@qq.com>
2022-09-26 17:46:27 +0800

    Modify SQL最流行50道题学完可以巩固几乎所有的查询推荐分享.sql

--- SQL最流行50道题学完可以巩固几乎所有的查询推荐分享.sql
+++ SQL最流行50道题学完可以巩固几乎所有的查询推荐分享.sql
@@ -81,3 +81,101 @@ and b.c_id='02'
 and a.s_id=b.s_id
 and a.s_score>b.s_score
 and a.s_id=c.s_id
+
+-- 长表转宽表、数据透视
+select
+	s.*,
+	t.s01,t.s02
+from
+	(select
+		a.s_id,
+		MAX(case when a.c_id='01' then a.s_score end) s01,
+		MAX(case when a.c_id='02' then a.s_score end) s02
+	from
+		Score a
+	group by a.s_id) t,Student s
+where t.s01>t.s02
+and s.s_id=t.s_id
+
+--2、查询“01”课程比“02”课程成绩低的学生信息及课程分数
+select
+	s.*,
+	t.s01,t.s02
+from
+	(select
+		a.s_id,
+		MAX(case when a.c_id='01' then a.s_score end) s01,
+		MAX(case when a.c_id='02' then a.s_score end) s02
+	from
+		Score a
+	group by a.s_id) t,Student s
+where t.s01<t.s02
+and s.s_id=t.s_id
+
+--3、查询平均成绩大于等于60分的同学的学生编号、学生姓名和平均成绩
+-- 子查询
+select
+	a.s_id,
+	(select s.s_name from Student s where s.s_id=a.s_id) s_name,
+	AVG(s_score) avg_s
+from
+	Score a
+group by
+	a.s_id
+having
+	AVG(a.s_score)>60
+
+-- 两个表连接
+select
+	a.s_id,s.s_name,
+	AVG(a.s_score) avg_s
+from
+	Score a,Student s
+where
+	a.s_id=s.s_id
+group by
+	a.s_id,s.s_name
+having
+	AVG(a.s_score)>=60
+
+--3、查询平均成绩小于60分的同学的学生编号、学生姓名和平均成绩
+select
+	a.s_id,s.s_name,
+	AVG(a.s_score) avg_s
+from
+	Score a,Student s
+where
+	a.s_id=s.s_id
+group by
+	a.s_id,s.s_name
+having
+	AVG(a.s_score)<60
+
+
+--4、查询所有同学的学生编号、学生姓名、选课总数、所有课程的总成绩
+select
+	a.s_id,
+	s.s_name,
+	COUNT(a.c_id) cnt_c,
+	SUM(a.s_score) sum_s
+from
+	Score a, Student s
+where
+	a.s_id=s.s_id
+group by
+	a.s_id,s.s_name
+
+
+select
+	a.s_id,
+	s.s_name,
+	COUNT(a.c_id) cnt_c,
+	SUM(a.s_score) sum_s
+from
+	Score a
+right join
+	Student s
+on
+	a.s_id=s.s_id
+group by
+	a.s_id,s.s_name
