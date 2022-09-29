27515cdc33ed499877464da4b859acc4a1808ecb
lstyxr <526560086@qq.com>
2022-09-29 17:40:15 +0800

    Modify SQL最流行50道题学完可以巩固几乎所有的查询推荐分享.sql

--- SQL最流行50道题学完可以巩固几乎所有的查询推荐分享.sql
+++ SQL最流行50道题学完可以巩固几乎所有的查询推荐分享.sql
@@ -330,3 +330,178 @@ where
 	s.s_id=t.s_id
 and t.s01 is not null
 and t.s02 is null
+
+--11、查询没有学全所有课程的学生信息
+--course score student
+select * from Student where exists(
+	select * from
+		(select
+			a.s_id,
+			COUNT(b.c_id) cnt_c
+		from
+			Student a
+		left join
+			Score b
+		on a.s_id=b.s_id
+		group by
+			a.s_id
+		having
+			COUNT(b.c_id)<
+			(select COUNT(c_id) from Course)) t
+	where
+		t.s_id=Student.s_id)
+
+--12、查询至少有一门课与学号为‘01’的同学所学相同的学生信息
+--in方法，distinct去重
+select
+	distinct a.*
+from
+	Student a
+left join
+	Score b
+on a.s_id=b.s_id
+where b.c_id in
+	(select c_id from Score where s_id='01')
+--in方法，分组去重
+select
+	a.*
+from
+	Student a
+left join
+	Score b
+on a.s_id=b.s_id
+where b.c_id in(
+	select c_id from Score where s_id='01')
+group by a.s_id,a.s_name,a.s_birth,a.s_sex
+
+--exists方法
+select
+	distinct a.*
+from
+	Student a
+left join
+	Score b
+on
+	a.s_id=b.s_id
+where exists(
+	select * from
+		(select c_id from Score where s_id='01') t
+	where
+		t.c_id=b.c_id)
+
+--查询和‘01’号同学所学完全相同的其他同学的信息
+--构建假设和‘01’所学都相同的临时表
+--full join方法
+select * from Student 
+where s_id not in(
+	select t2.s_id 
+	from
+		(select
+			t.*,s.c_id cid2
+		from
+			(select
+				a.*,b.c_id
+			from
+				Student a,
+				(select c_id from Score where s_id='01') b) t
+		full join
+			Score s
+		on  t.s_id=s.s_id
+		and s.c_id=t.c_id) t2
+	where t2.cid2 is null or t2.c_id is null)
+and s_id!='01'
+
+--如果不支持full join方法，只能分别左右连接再union
+with tmpT as(
+select
+	t.*,s.c_id cid2
+from
+	(select
+		a.*,b.c_id
+	from
+		Student a,
+		(select * from Score where s_id='01') b) t
+right join
+	Score s
+on  s.s_id=t.s_id
+and s.c_id=t.c_id
+union
+select
+	t.*,s.c_id cid2
+from
+	(select
+		a.*,b.c_id
+	from
+		Student a,
+		(select * from Score where s_id='01') b) t
+left join
+	Score s
+on  s.s_id=t.s_id
+and s.c_id=t.c_id)
+select * from Student where s_id not in(
+select s_id from tmpT where c_id is null or cid2 is null)
+
+--14、查询没学过‘张三’老师讲授的任一门课程的学生姓名
+select s_name from Student where not exists(
+	select * from
+		(select
+			s.s_id
+		from
+			Score s
+		where s.c_id in(
+			select 
+				a.c_id
+			from
+				Course a,Teacher b
+			where
+				a.t_id=b.t_id
+			and b.t_name='张三')) t
+	where t.s_id=Student.s_id)
+
+--15、查询两门及以上不及格课程的学生学号、姓名及其平均成绩
+--自创
+select
+	*
+from
+	(select
+		s.s_id,s.s_name,t2.avg_s
+	from
+		Student s
+	left join
+		(select
+			t.s_id,
+			AVG(t.s_score) avg_s
+		from
+			(select
+				*
+			from
+				Score a
+			where
+				a.s_score<60) t
+		group by
+			t.s_id) t2
+	on t2.s_id=s.s_id) t3
+where t3.avg_s is not null
+
+--老师
+select
+	s.s_id,s.s_name,AVG(a.s_score) avg_s,
+	sum(case when a.s_score>=60 then 0 else 1 end) 不及格门数
+from
+	Student s
+left join
+	Score a
+on  a.s_id=s.s_id
+group by
+	s.s_id,s.s_name
+having
+	sum(case when a.s_score>=60 then 0 else 1 end)>=2
+
+--case when的理解，实际是生成新的一列，0代表及格、1代表不及格，再用sum函数求总的不及格门数
+select
+	s_id,
+	sum(case when s_score>=60 then 0 else 1 end) sum_jg
+from
+	Score
+group by
+	s_id
